5c8be330dea8b632cbc292723eaf486b1daf9fee
Sebastian-Clarifruit <127881456+Sebastian-Clarifruit@users.noreply.github.com>
2024-03-11 20:01:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import string
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 @app.route('/test')
 def barcode_info():
@@ -16,129 +18,130 @@ def barcode_info():
         letters = string.digits
         name = "demo " + (''.join(random.choice(letters) for i in range(6)))
         return {
-                    "inspectionName": name,
-                    "ggnumber": 987654321,
-                    "itemweight": 500,
-                    "grower": "Fran",
-                    "coo": "Mexico",
-                    "codeVariety": "Thompson",
-                    "processKey": "GRAPES",
-                    "standardKey": "DEFAULT"
-                }
-        
+            "inspectionName": name,
+            "ggnumber": 987654321,
+            "itemweight": 500,
+            "grower": "Fran",
+            "coo": "Mexico",
+            "codeVariety": "Thompson",
+            "processKey": "GRAPES",
+            "standardKey": "DEFAULT"
+        }
+
     if barcode == "Nature Fresh":
         letters = string.digits
         name = "Nature Fresh Trial" + (''.join(random.choice(letters) for i in range(6)))
         return {
-                    "inspectionName": name,
-                    "coo": "USA",
-                    "warehouse": "NFFSALESDC",
-                    "insty": " Entrance Control",
-                    "shipmen": "Conventional",
-                    "insppo": "Arrival",
-                    "shi": "N/A",
-                    "shire": 12345,
-                    "cnee": "Nature Fresh Farms",
-                    "cneeref": 1111,
-                    "loca": "NFFSALESDC, Production",
-                    "ve": "Maersk",
-                    "sample": 100,
-                    "codeVariety": "BeefSteak",
-                    "processKey": "lot",
-                    "standardKey": "DEFAULT"
-                }
-            
-        
+            "inspectionName": name,
+            "coo": "USA",
+            "warehouse": "NFFSALESDC",
+            "insty": " Entrance Control",
+            "shipmen": "Conventional",
+            "insppo": "Arrival",
+            "shi": "N/A",
+            "shire": 12345,
+            "cnee": "Nature Fresh Farms",
+            "cneeref": 1111,
+            "loca": "NFFSALESDC, Production",
+            "ve": "Maersk",
+            "sample": 100,
+            "codeVariety": "BeefSteak",
+            "processKey": "lot",
+            "standardKey": "DEFAULT"
+        }
+
     if barcode == "Bjs Grapes":
         letters = string.digits
         name = "Bjs Grapes" + (''.join(random.choice(letters) for i in range(6)))
         return {
-                    "inspectionName": name,
-                    "COO": "USA",
-                    "PD": "1709607600000",
-                    "IN": "BJ's",
-                    "codeVariety": "CO",
-                }
+            "inspectionName": name,
+            "COO": "USA",
+            "PD": "1709607600000",
+            "IN": "BJ's",
+            "codeVariety": "CO",
+        }
     if barcode == "Bjs Oranges":
         letters = string.digits
         name = "Bjs Oranges" + (''.join(random.choice(letters) for i in range(6)))
         return {
-                    "inspectionName": name,
-                    "COO": "USA",
-                    "PD": "1709607600000",
-                    "IN": "BJ's",
-                    "codeVariety": "CO",
-                }
+            "inspectionName": name,
+            "COO": "USA",
+            "PD": "1709607600000",
+            "IN": "BJ's",
+            "codeVariety": "CO",
+        }
     if barcode == "abcd1234":
         letters = string.digits
         name = "FDM" + (''.join(random.choice(letters) for i in range(6)))
         return {
-                    "inspectionName": name,
-                    "coo": "Costa Rica",
-                    "arrivaldate": "1709607600000",
-                    "grower": "FDM",
-                }
+            "inspectionName": name,
+            "coo": "Costa Rica",
+            "arrivaldate": "1709607600000",
+            "grower": "FDM",
+        }
     if barcode == "21002754":
         letters = string.digits
         name = barcode
         return {
-                    "inspectionName": name,
-                    "exp": "SOC. COOP. AGR. ZEOLI FRUIT",
-                    "coo": "ITALY",
-                    "vess": "MSC SAMANTHA",
-                    "cont": "MSDU 985028/4",
-                    "eta": "08-03-2024",
-                }
-   if barcode == "3620215":
+            "inspectionName": name,
+            "exp": "SOC. COOP. AGR. ZEOLI FRUIT",
+            "coo": "ITALY",
+            "vess": "MSC SAMANTHA",
+            "cont": "MSDU 985028/4",
+            "eta": "08-03-2024",
+        }
+
+
+    if barcode == "3620215":
         letters = string.digits
         name = barcode
         return {
-                    "inspectionName": name,
-                    "exp": "FRUITION SA",
-                    "coo": "GREECE",
-                    "vess": "MSC SAMANTHA",
-                    "cont": "TTNU 820993/1",
-                    "eta": "08-03-2024",
-                }
+            "inspectionName": name,
+            "exp": "FRUITION SA",
+            "coo": "GREECE",
+            "vess": "MSC SAMANTHA",
+            "cont": "TTNU 820993/1",
+            "eta": "08-03-2024",
+        }
     if barcode == "3550252":
         letters = string.digits
         name = barcode
         return {
-                    "inspectionName": name,
-                    "exp": "FRUTTA FRIULI SCA",
-                    "coo": "ITALY",
-                    "vess": "MSC NIOVI VIII",
-                    "cont": "MNBU 438520/2",
-                    "eta": "08-03-2024",
-                }
+            "inspectionName": name,
+            "exp": "FRUTTA FRIULI SCA",
+            "coo": "ITALY",
+            "vess": "MSC NIOVI VIII",
+            "cont": "MNBU 438520/2",
+            "eta": "08-03-2024",
+        }
     if barcode == "3620216":
         letters = string.digits
         name = barcode
         return {
-                    "inspectionName": name,
-                    "exp": "FRUITION SA",
-                    "coo": "GREECE",
-                    "vess": "MSC SAMANTHA",
-                    "cont": "TRIU 895471/2",
-                    "eta": "08-03-2024",
-                }
+            "inspectionName": name,
+            "exp": "FRUITION SA",
+            "coo": "GREECE",
+            "vess": "MSC SAMANTHA",
+            "cont": "TRIU 895471/2",
+            "eta": "08-03-2024",
+        }
     if barcode == "3420229":
         letters = string.digits
         name = barcode
         return {
-                    "inspectionName": name,
-                    "exp": "VITULA FRUIT SRL",
-                    "coo": "ITALY",
-                    "vess": "MSC SAMANTHA",
-                    "cont": "MSDU 983085/8",
-                    "eta": "08-03-2024",
-                }
-                
+            "inspectionName": name,
+            "exp": "VITULA FRUIT SRL",
+            "coo": "ITALY",
+            "vess": "MSC SAMANTHA",
+            "cont": "MSDU 983085/8",
+            "eta": "08-03-2024",
+        }
+    
     else:
-        return jsonify({"error":"No Stock - Barcode not exist",
-                        "message":"There is no stock on pallet reference"}), 422
-                        
-                                                            
+        return jsonify({"error": "No Stock - Barcode not exist",
+                        "message": "There is no stock on pallet reference"}), 422
+
+
 @app.route('/foo', methods=['POST'])
 def print_payload():
     payload = request.get_json()
@@ -151,4 +154,4 @@ def print_payload():
 @app.route('/barcode_issue')
 def index():
     return jsonify("Error: Barcode is not exists"), 422
-    
+
