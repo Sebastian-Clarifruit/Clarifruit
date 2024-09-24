01cb85de366dbc9794af8c3293c967d1f2be1576
Sebastian-Clarifruit <127881456+Sebastian-Clarifruit@users.noreply.github.com>
2024-09-24 14:31:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
-from flask import Flask, request, jsonify
-import csv
+from flask import Flask, request, json, jsonify
+import time
+import random
+import string
 
 app = Flask(__name__)
 
-# Load CSV data into memory for quick access
-data = []
-with open('your_file.csv', newline='', encoding='utf-8') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        data.append(row)
+def hello_world():
+    return 'Hello, World!'
 
-@app.route('/get_info', methods=['GET'])
-def get_info():
-    pallet_number = request.args.get('Pallet#')
-    
-    if not pallet_number:
-        return jsonify({"error": "Pallet# is required"}), 400
-    
-    # Search for the pallet number in the data
-    for row in data:
-        if row['Pallet#'] == pallet_number:
-            return jsonify(row), 200  # Directly return the row dictionary
-    
-    # If the pallet number is not found
-    return jsonify({"error": "Pallet# not found"}), 404
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/test')
+def barcode_info():
+    barcode = request.args.get('barcode')
+    
+  
+
+    if barcode == "abcd1234":
+        letters = string.digits
+        name = "Strawberry Fields"
+        return {
+            "inspectionName": name,
+            "coo": "USA",
+            "grower": "Field 1",
+            "lot": "2404",
+            "packdate": "1709607600000",       
+        }
+    else:
+        return jsonify({"error": "No Stock - Barcode not exist",
+                        "message": "There is no stock on pallet reference"}), 422
+
+
+@app.route('/foo', methods=['POST'])
+def print_payload():
+    payload = request.get_json()
+    print(payload)
+    return 'Payload received and printed'
+    if __name__ == '__main__':
+        app.run()
+
+
+@app.route('/barcode_issue')
+def index():
+    return jsonify("Error: Barcode is not exists"), 422
